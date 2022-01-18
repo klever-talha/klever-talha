87af3af7dbca597a786067d6c682d2b98f22cc83
klevertalha@gmail.com <klevertalha@gmail.com>
2022-01-18 14:52:48 +0500

    init

--- js/3d.js
+++ js/3d.js
@@ -1,43 +1,24 @@
 /* Importing Three Js library */
 import * as THREE from 'https://cdn.skypack.dev/three@0.133.1';
 import {GLTFLoader} from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/loaders/GLTFLoader.js';
-import {OrbitControls} from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls.js';
 
-const container = document.querySelector('.scene');
+var scene = new THREE.Scene();
 
-const scene = new THREE.Scene();
-
-//  camera
-const camera = new THREE.PerspectiveCamera(18, container.clientWidth / container.clientHeight, 0.1, 1000);
-camera.position.set(0, 0, 2);
-
-// renderer
-const renderer = new THREE.WebGLRenderer({
-   alpha: true,
-   antialias: true
-});
-renderer.setSize(container.clientWidth, container.clientHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-container.appendChild(renderer.domElement);
-
-// OrbitControls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.rotateSpeed = 0.09;
-controls.enableZoom = false;
-controls.enablePan = false;
-
-container.addEventListener("mouseup", function () {
-   controls.reset();
+//renderer
+var renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  alpha: true
 });
 
-// GLTFLoader
-const loader = new GLTFLoader();
-loader.load("res/scene.glb", function (gltf) {
-   scene.add(gltf.scene);
-});
+//camera
+var camera = new THREE.PerspectiveCamera(10, 1, 1, 1000);
+camera.position.set(0, 0, 3.2);
 
-// Lights
+//canvas -- > append in html
+var canvas = renderer.domElement;
+document.querySelector('#character').appendChild(canvas);
 
+//light
 const light = new THREE.AmbientLight( 0x222222, 2.5 ); // soft white light
 scene.add( light );
 
@@ -46,9 +27,48 @@ spotLight.position.set( 0.788, 1.547, 6.26 );
 spotLight.castShadow = false;
 scene.add( spotLight );
 
-function animate() {
-   requestAnimationFrame(animate);
-   renderer.render(scene, camera);
+//gltf loader
+const loader = new GLTFLoader();
+loader.load( 'res/scene.glb', function ( gltf ) {
+  base.add( gltf.scene );
+} );
+
+
+let base = new THREE.Object3D();
+scene.add(base);
+
+var plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -2);
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var pointOfIntersection = new THREE.Vector3();
+window.addEventListener("mousemove", onMouseMove, false);
+
+function onMouseMove(event){
+  mouse.x = ( event.clientX / window.innerWidth ) * 12 - 6;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 12 + 6;
+  raycaster.setFromCamera(mouse, camera);
+  raycaster.ray.intersectPlane(plane, pointOfIntersection);
+  base.lookAt(pointOfIntersection);
+  console.log(mouse.x)
+  console.log(mouse.y)
 }
 
-animate();
+renderer.setAnimationLoop(() => {
+  if (resize(renderer)) {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+  renderer.render(scene, camera);
+});
+
+function resize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
